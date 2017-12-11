4234e0bd7f469bb5a477f6ae4775f952266ef922
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2017-12-11 15:37:27 -0500

    Added position noise

--- gym_duckietown/envs/__init__.py
+++ gym_duckietown/envs/__init__.py
@@ -1,2 +1,2 @@
 from gym_duckietown.envs.duckietown_env import DuckietownEnv, DiscreteEnv
-from gym_duckietown.envs.simplesim_env import SimpleSimEnv, SimpleSimDscEnv
+from gym_duckietown.envs.simplesim_env import SimpleSimEnv
