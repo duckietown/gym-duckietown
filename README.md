f9053d83a026915f6196b0b37647d2a217ead817
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2017-12-22 18:08:31 -0500

    Moved custom Pytorch RL (model) code into `basicrl` to keep it in sync.

--- README.md
+++ README.md
@@ -28,6 +28,29 @@ cd gym
 pip3 install -e .
 ```
 
+Reinforcement learning code forked from [this repository](https://github.com/ikostrikov/pytorch-a2c-ppo-acktr)
+is included under [/basicrl](/basicrl). If you wish to use this code, you
+can install its dependencies as follows:
+
+```
+cd basicrl
+
+# PyTorch
+conda install pytorch torchvision -c soumith
+
+# Dependencies needed by OpenAI baselines
+sudo apt install libopenmpi-dev zlib1g-dev cmake
+
+# OpenAI baselines
+git clone https://github.com/openai/baselines.git
+cd baselines
+pip3 install -e .
+cd ..
+
+# Other requirements
+pip3 install -r requirements.txt
+```
+
 Usage
 -----
 
@@ -37,21 +60,23 @@ To run the standalone UI application, which allows you to control the robot manu
 ./standalone.py
 ```
 
-The standalone application will connect to the ROS bridge node, display
-camera images received and send actions (keyboard commands) back. By
-default, the ROS bridge is assumed to be running on localhost.
+The standalone application will launch the gym environment, receive
+camera images received and send actions (keyboard commands) back.
 
-To train a reinforcement learning agent, you can clone the PyTorch code from [this repository](https://github.com/maximecb/pytorch-a2c-ppo-acktr). It has been modified and tested for
-compatibility with `gym-duckietown`. I recommend using the PPO or ACKTR implementations.
+To train a reinforcement learning agent, you can use the code provided under [/basicrl](/basicrl). I recommend using the A2C or ACKTR implementations.
 A sample command to launch training is:
 
 ```
-python3 main.py --env-name Duckietown-v0 --no-vis --num-processes 1 --algo acktr  --num-frames 10000000 --entropy-coef 0.22 --lr 0.0002
+python3 basicrl/main.py --env-name Duckie-SimpleSim-Discrete-v0 --no-vis --num-processes 1 --algo acktr --num-frames 5000000 --entropy-coef 0.22 --lr 0.0002
 ```
 
 Then, to visualize the result of training, you can run the following command.
 Note that you can do this while the training process is still ongoing.
 
 ```
-python3 enjoy.py --env-name Duckie-SimpleSim-Discrete-v0 --num-stack 4 --load-dir trained_models/acktr
+python3 basicrl/enjoy.py --env-name Duckie-SimpleSim-Discrete-v0 --num-stack 4 --load-dir trained_models/acktr
 ```
+
+Note that reinforcement learning algorithms are extremely sensitive to hyperparameters. Choosing the
+wrong set of parameters could prevent convergence completely, or lead to unstable performance over
+training. You will likely want to experiment.
---
+++ basicrl/envs.py
@@ -0,0 +1,95 @@
+import os
+import numpy
+import gym
+import random
+
+from gym.spaces.box import Box
+
+from baselines import bench
+from baselines.common.atari_wrappers import make_atari, wrap_deepmind
+
+try:
+    import pybullet_envs
+except ImportError:
+    pass
+
+try:
+    import gym_aigame
+except ImportError:
+    pass
+
+try:
+    import gym_duckietown
+    from gym_duckietown.envs import *
+except ImportError:
+    pass
+
+
+def make_env(env_id, seed, rank, log_dir, start_container):
+    def _thunk():
+        if env_id.startswith('Duckietown'):
+            basePort = DuckietownEnv.SERVER_PORT
+            if start_container:
+                basePort += random.randint(0, 20000)
+            env = DuckietownEnv(
+                serverPort= basePort + rank,
+                startContainer=start_container
+            )
+            if "Discrete" in env_id:
+                env = DiscreteEnv(env)
+        elif "SimpleSim" in env_id:
+            env = SimpleSimEnv()
+            if "Discrete" in env_id:
+                env = DiscreteEnv(env)
+        else:
+            env = gym.make(env_id)
+            env = DiscreteEnv(env)
+
+        is_atari = hasattr(gym.envs, 'atari') and isinstance(env.unwrapped, gym.envs.atari.atari_env.AtariEnv)
+        if is_atari:
+            env = make_atari(env_id)
+        env.seed(seed + rank)
+        if log_dir is not None:
+            env = bench.Monitor(env, os.path.join(log_dir, str(rank)))
+        if is_atari:
+            env = wrap_deepmind(env)
+
+        # If the input has shape (W,H,3), wrap for PyTorch convolutions
+        obs_shape = env.observation_space.shape
+        if len(obs_shape) == 3 and obs_shape[2] == 3:
+            env = WrapPyTorch(env)
+
+        env = ScaleObservations(env)
+
+        return env
+
+    return _thunk
+
+
+class ScaleObservations(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(ScaleObservations, self).__init__(env)
+        self.obs_lo = self.observation_space.low[0,0,0]
+        self.obs_hi = self.observation_space.high[0,0,0]
+        obs_shape = self.observation_space.shape
+        self.observation_space = Box(0.0, 1.0, obs_shape)
+
+    def _observation(self, obs):
+        if self.obs_lo == 0.0 and self.obs_hi == 1.0:
+            return obs
+        else:
+            return (obs - self.obs_lo) / (self.obs_hi - self.obs_lo)
+
+
+class WrapPyTorch(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(WrapPyTorch, self).__init__(env)
+        obs_shape = self.observation_space.shape
+        self.observation_space = Box(
+            self.observation_space.low[0,0,0],
+            self.observation_space.high[0,0,0],
+            [obs_shape[2], obs_shape[1], obs_shape[0]]
+        )
+
+    def _observation(self, observation):
+        return observation.transpose(2, 0, 1)
