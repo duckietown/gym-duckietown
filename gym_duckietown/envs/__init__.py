271c9570ffacebeab425e6c972fffd92cadabe02
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2017-12-24 11:41:00 -0500

    Implemented heading wrapper

--- gym_duckietown/envs/__init__.py
+++ gym_duckietown/envs/__init__.py
@@ -1,3 +1,3 @@
-from gym_duckietown.envs.duckietown_env import DuckietownEnv, DiscreteEnv
+from gym_duckietown.envs.duckietown_env import DuckietownEnv, DiscreteWrapper, HeadingWrapper
 from gym_duckietown.envs.simplesim_env import SimpleSimEnv
 from gym_duckietown.envs.duckiebot_env import DuckiebotEnv
