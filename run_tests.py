9b41f34e1017642b17bedd80651c4a33d43f8ea6
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-05-18 16:00:47 -0400

    Added very basic image comparison test

--- run_tests.py
+++ run_tests.py
@@ -5,6 +5,14 @@ import gym
 import gym_duckietown
 
 env = gym.make('SimpleSim-v0')
-env.reset()
+
+first_obs = env.reset()
+
+# Check that the human rendering resembles the agent's view
+first_render = env.render('rgb_array')
+m0 = first_obs.mean()
+m1 = first_render.mean()
+assert m0 > 0 and m0 < 255
+assert abs(m0 - m1) < 5
 
 obs, _, _, _ = env.step(np.array([0.1, 0.1]))
