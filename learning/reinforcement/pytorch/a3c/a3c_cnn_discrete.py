cfee873dd9d961e106f5773443104be823895c92
DDreher <dd058@hdm-stuttgart.de>
2019-06-22 12:44:25 +0200

    fix entropy term

--- learning/reinforcement/pytorch/a3c/a3c_cnn_discrete.py
+++ learning/reinforcement/pytorch/a3c/a3c_cnn_discrete.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.multiprocessing as mp
-
+from torch.autograd import Variable
 import numpy as np
 
 
@@ -24,14 +24,14 @@ class Net(nn.Module):
 
         # Conv Filter output size:
         # o = output
+        # o = [i + 2*p - k - (k-1)*(d-1)]/s + 1
         # p = padding
         # k = kernel_size
         # s = stride
         # d = dilation
 
-        # o = [i + 2*p - k - (k-1)*(d-1)]/s + 1
-
-        self.conv1 = nn.Conv2d(in_channels=self.channels, out_channels=16, kernel_size=3, stride=2, padding=1)  # (16,30,40)
+        self.conv1 = nn.Conv2d(in_channels=self.channels, out_channels=16, kernel_size=3, stride=2,
+                               padding=1)  # (16,30,40)
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=2, padding=1)  # (16,15,20)
 
         self.dense_size = int(16 * (60 * .5 * .5) * (80 * .5 * .5))
@@ -42,8 +42,6 @@ class Net(nn.Module):
         for layer in [self.conv1, self.conv2, self.pi, self.v]:
             nn.init.normal_(layer.weight, mean=0., std=0.1)
             nn.init.constant_(layer.bias, 0.)
-
-        self.distribution = torch.distributions.Categorical
 
     def forward(self, x):
         x = F.elu(self.conv1(x))
@@ -59,20 +57,19 @@ class Net(nn.Module):
         s = v_wrap(s)
         s.unsqueeze_(0)
         logits, _ = self.forward(s)
-        prob = F.softmax(logits, dim=1).data
-        m = self.distribution(prob)
-        return m.sample().numpy()[0]
+        probabilities = F.softmax(logits, dim=1)
+        action_distribution = self.distribution(probabilities)
+        return action_distribution.sample().numpy()[0]
 
     def loss_func(self, s, a, v_t):
         self.train()
         logits, values = self.forward(s)
-        td = v_t - values
+        td = v_t - values  # Temporal Difference as in DQN
         critic_loss = td.pow(2)
 
-        probs = F.softmax(logits, dim=1)
-        m = self.distribution(probs)
-        exp_v = m.log_prob(a) * td.detach().squeeze()
-        a_loss = -exp_v
+        probabilities = F.softmax(logits, dim=1)
+        action_distribution = self.distribution(probabilities)
+        a_loss = -action_distribution.log_prob(a) * td.detach().squeeze()
         total_loss = (critic_loss + a_loss).mean()
 
         return total_loss
@@ -102,25 +99,29 @@ class Worker(mp.Process):
         self.env = gym.make(self.env_name).unwrapped
 
         self.shape_obs_space = self.env.observation_space.shape
-        self.shape_action_space = self.env.action_space.n
+        self.num_actions = self.env.action_space.n
 
-        self.local_net = Net(1, self.shape_action_space)  # local network
+        self.local_net = Net(1, self.num_actions)  # local network, no shared weights
 
+        # Start training
         while self.global_episode.value < self.max_episodes:
 
-            # sync global parameters
-            self.local_net.load_state_dict(self.global_net.state_dict())
+            # init
+            obs = torch.from_numpy(preprocess_state(self.env.reset()))
 
-            obs = preprocess_state(self.env.reset())
-
-            buffer_states = []
-            buffer_actions = []
-            buffer_rewards = []
+            values = []
+            log_probs = []
+            rewards = []
+            entropies = []
 
             t = 0
             episode_reward = 0
             done = False
 
+            # sync local model with shared model
+            self.local_net.load_state_dict(self.global_net.state_dict())
+
+            # Play an episode
             while done is False:
                 t += 1
                 self.total_step += 1
@@ -128,85 +129,86 @@ class Worker(mp.Process):
                 if self.name == "Worker 0" and self.graphical_output:
                     self.env.render()
 
-                action = self.local_net.choose_action(obs)
-                # print('Chosen action:', action)
-                new_obs, reward, done, info = self.env.step(action)
-                new_obs = preprocess_state(new_obs)
+                logits, value = self.local_net.forward(Variable(obs.unsqueeze(0)))
 
-                episode_reward += reward
+                prob = F.softmax(logits, dim=0)
+                log_prob = F.log_softmax(logits, dim=0)
 
-                buffer_states.append(obs)
-                buffer_actions.append(action)
-                buffer_rewards.append(reward)
+                entropy = -(log_prob * prob).sum(1, keepdim=True)
+                entropies.append(entropy)
 
-                if t == self.max_steps_per_episode:
-                    with self.global_episode.get_lock():
-                        self.global_episode.value += 1
-                    done = True
+                action = prob.multinomial(num_samples=1).data
+                log_prob = log_prob.gather(1, Variable(action))
 
-                # Sync local net and global net
-                if self.total_step % self.update_global_net_frequency == 0 or done:
-                    # print(self.name + ': Syncing nets')
+                # Step environment
+                obs, reward, done, _ = self.env.step(action.numpy())
+                obs = torch.from_numpy(preprocess_state(obs))
 
-                    sync_nets(self.optimizer, self.local_net, self.global_net, done, new_obs, buffer_states,
-                              buffer_actions, buffer_rewards, self.gamma)
+                done = done or t >= self.max_steps_per_episode
 
-                    buffer_states = []
-                    buffer_actions = []
-                    buffer_rewards = []
+                # Clamp reward to [-1,1]
+                episode_reward += max(min(reward, 1), -1)
 
-                    if done:
-                        with self.global_episode.get_lock():
-                            self.global_episode.value += 1
+                # Book keeping
+                values.append(value)
+                log_probs.append(log_prob)
+                rewards.append(episode_reward)
+            # --- End Episode
 
-                        with self.global_episode_reward.get_lock():
-                            # Moving average
-                            if self.global_episode_reward.value == 0.:
-                                self.global_episode_reward.value = episode_reward
-                            else:
-                                self.global_episode_reward.value = self.global_episode_reward.value * 0.9 + \
-                                                                   episode_reward * 0.1
-                        self.res_queue.put(self.global_episode_reward.value)
+            # The episode has ended but we still have to add the terminal reward.
+            R = torch.zeros(1, 1)
+            if not done:
+                _, value = self.local_net.forward(Variable(obs.unsqueeze(0)))
+                R = value.data
+            values.append(Variable(R))
 
-                        print(self.name, 'Global Episode:', self.global_episode.value,
-                              '| Global Episode R: %.0f' % self.global_episode_reward.value)
-                        break
+            policy_loss = 0
+            value_loss = 0
 
-                obs = new_obs
+            R = Variable(R)
+
+            gae = torch.zeros(1, 1)
+            for i in reversed(range(len(rewards))):
+                R = self.gamma * R + rewards[i]
+                advantage = R - values[i]
+                value_loss += 0.5 * advantage.pow(2)
+
+                # Generalized Advantage Estimation (gae)
+                delta_t = rewards[i] + self.gamma * values[i + 1].data - values[i].data
+                gae = gae * self.gamma + delta_t
+
+                policy_loss += -(log_probs[i] * Variable(gae) + 0.01 * entropies[i])
+
+            self.optimizer.zero_grad()
+            (policy_loss + 0.5 * value_loss).backward()
+            torch.nn.utils.clip_grad_norm(self.local_net.parameters(), 40)
+
+            ensure_shared_grads(self.local_net, self.global_net)
+
+            self.optimizer.step()
+
+            # Book keeping of the training statistics
+            with self.global_episode.get_lock():
+                self.global_episode.value += 1
+
+            with self.global_episode_reward.get_lock():
+                # Moving average
+                if self.global_episode_reward.value == 0.:
+                    self.global_episode_reward.value = episode_reward
+                else:
+                    self.global_episode_reward.value = self.global_episode_reward.value * 0.9 + \
+                                                       episode_reward * 0.1
+            self.res_queue.put(self.global_episode_reward.value)
+
+            print(self.name, 'Global Episode:', self.global_episode.value,
+                  '| Global Episode R: %.0f' % self.global_episode_reward.value)
+
         self.env.close()
         self.res_queue.put(None)
 
 
-def sync_nets(optimizer, local_net, global_net, done, next_state, state_buffer, action_buffer, reward_buffer, gamma):
-    if done:
-        v_next_state = 0.  # terminal
-    else:
-        flattened_input = v_wrap(next_state[None, :])
-        v_next_state = local_net.forward(flattened_input)[-1].data.numpy()[0, 0]
-
-    buffer_v_target = []
-    for r in reward_buffer[::-1]:  # reverse buffer r
-        v_next_state = r + gamma * v_next_state
-        buffer_v_target.append(v_next_state)
-    buffer_v_target.reverse()
-
-    loss = local_net.loss_func(
-        v_wrap(np.array(state_buffer)),
-        v_wrap(np.array(action_buffer), dtype=np.int64) if action_buffer[0].dtype == np.int64 else v_wrap(
-            np.vstack(action_buffer)),
-        v_wrap(np.array(buffer_v_target)[:, None]))
-
-    # calculate local gradients
-    optimizer.zero_grad()
-    loss.backward()
-
-    # push local parameters to global
-    for lp, gp in zip(local_net.parameters(), global_net.parameters()):
-        gp._grad = lp.grad
-    optimizer.step()
-
-
-def v_wrap(np_array, dtype=np.float32):
-    if np_array.dtype != dtype:
-        np_array = np_array.astype(dtype)
-    return torch.from_numpy(np_array)
+def ensure_shared_grads(model, shared_model):
+    for param, shared_param in zip(model.parameters(), shared_model.parameters()):
+        if shared_param.grad is not None:
+            return
+        shared_param._grad = param.grad
