8d29af2bcc5fc72779a0c6aa1a4b73788f4db784
ddreher <blubbasd@googlemail.com>
2019-06-26 19:14:55 +0200

    Update inference script to new net architecture

--- learning/reinforcement/pytorch/a3c/enjoy_reinforcement.py
+++ learning/reinforcement/pytorch/a3c/enjoy_reinforcement.py
@@ -5,27 +5,34 @@ import os
 import numpy as np
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.multiprocessing as mp
 
 # Duckietown Specific
 import gym
-from learning.reinforcement.pytorch.a3c import a3c_continuous_simple as a3c
+from learning.reinforcement.pytorch.a3c import a3c_cnn_discrete_gru as a3c
 from learning.utils.env import launch_env
 from learning.utils.wrappers import NormalizeWrapper, ImgWrapper, \
-    DtRewardWrapper, ActionWrapper, ResizeWrapper, ActionClampWrapper
+    DtRewardWrapper, ActionWrapper, ResizeWrapper, DiscreteWrapper
+
+
+def preprocess_state(obs):
+    from scipy.misc import imresize
+    return imresize(obs.mean(2), (80, 80)).astype(np.float32).reshape(1, 80, 80) / 255.
 
 
 def _enjoy(args):
-    env = gym.make('Duckietown-udem1-v0')
-    env = ResizeWrapper(env)
-    env = NormalizeWrapper(env)
+    env = launch_env()
+    # env = ResizeWrapper(env)
+    # env = NormalizeWrapper(env)
     env = ImgWrapper(env)  # to make the images from 160x120x3 into 3x160x120
-    env = ActionWrapper(env)
-    env = ActionClampWrapper(env)  # clip to range [-1, 1]
-    #env = PreventBackwardsWrapper(env)  # prevent duckiebot driving backwards..
+    # env = ActionWrapper(env)
     env = DtRewardWrapper(env)
+    env = DiscreteWrapper(env)
 
     shape_obs_space = env.observation_space.shape  # (3, 120, 160)
-    shape_action_space = env.action_space.shape[0]  # (2,)
+    shape_action_space = env.action_space.n  # (2,)
 
     # Initialize policy
 
@@ -34,26 +41,42 @@ def _enjoy(args):
     path = os.path.join(cwd, args.model_dir, args.model_file)
     print('Loading model from:', path)
 
-    global_net = torch.load(path)
+    checkpoint = torch.load(path)
+    global_net = a3c.Net(channels=1, num_actions=shape_action_space)
+    # global_net.load_state_dict(checkpoint['model_state_dict'])
+    global_net.load_state_dict(checkpoint)
     global_net.eval()
 
-    #global_net.load_state_dict(state_dict.Net)
-
-    obs = env.reset()
-    done = False
+    state = torch.tensor(preprocess_state(env.reset()))
+    done = True
 
     while True:
-        while not done:
-            action = global_net.choose_action(np.array(obs))
+        with torch.no_grad():
+            if done:
+                hx = torch.zeros(1, 256)
+            else:
+                hx = hx.detach()
+
+            # Inference
+            value, logit, hx = global_net.forward((state.view(-1, 1, 80, 80), hx))
+            action_log_probs = F.log_softmax(logit, dim=-1)
+
+            # Take action with highest probability
+            action = action_log_probs.max(1, keepdim=True)[1].numpy()
+
             # Perform action
-            obs, reward, done, _ = env.step(action)
+            state, reward, done, _ = env.step(action)
+            state = torch.tensor(preprocess_state(state))
+
             env.render()
-        done = False
-        obs = env.reset()
+
+            if done:
+                state = torch.tensor(preprocess_state(env.reset()))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model-dir', type=str, default='models')  # Name of the directory where the models are saved
-    parser.add_argument('--model-file', type=str, default='2019-06-10_19-28-47_a3c-cont.pth')  # Name of the model file
+    parser.add_argument('--model-file', type=str,
+                        default='2019-06-26_13-34-15_a3c-disc-duckie.pth')  # Name of the model file
     _enjoy(parser.parse_args())
