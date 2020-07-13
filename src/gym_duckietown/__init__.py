dcf7f899e9469803117485e3b65273dc13f8a872
Anthony Courchesne <anthony.courchesne@mail.mcgill.ca>
2020-07-13 14:34:48 -0400

    Reduce default log level

--- src/gym_duckietown/__init__.py
+++ src/gym_duckietown/__init__.py
@@ -5,7 +5,7 @@ import logging
 
 logging.basicConfig()
 logger = logging.getLogger('gym-duckietown')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 logger.info('gym-duckietown %s\n' % __version__)
 
