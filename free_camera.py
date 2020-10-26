ad42a5a5e6f883ee9de3da83f089bc1be4250937
Anthony Courchesne <anthony.courchesne@mail.mcgill.ca>
2020-10-26 11:41:44 -0400

    Make the argument a list with different values

--- free_camera.py
+++ free_camera.py
@@ -23,7 +23,7 @@ parser.add_argument('--draw-bbox', action='store_true', help='draw collision det
 parser.add_argument('--domain-rand', action='store_true', help='enable domain randomization')
 parser.add_argument('--frame-skip', default=1, type=int, help='number of frames to skip')
 parser.add_argument('--distortion', default=False,  action='store_true')
-parser.add_argument('--synth-tiles', default=False, action='store_true')
+parser.add_argument('--style', default="photos", choices=['photos','synthetic'])
 args = parser.parse_args()
 
 if args.env_name is None:
@@ -34,7 +34,7 @@ if args.env_name is None:
         domain_rand=args.domain_rand,
         frame_skip=args.frame_skip,
         distortion=args.distortion,
-        synthetic_tiles=args.synth_tiles
+        style=args.style
     )
 else:
     env = gym.make(args.env_name)
