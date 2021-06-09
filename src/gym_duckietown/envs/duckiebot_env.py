9746c813ceff98ca488ddd913c7ad21ddc379666
Andrea Censi <acensi@ethz.ch>
2021-06-09 08:57:01 +0200

    reformat

--- src/gym_duckietown/envs/duckiebot_env.py
+++ src/gym_duckietown/envs/duckiebot_env.py
@@ -168,7 +168,13 @@ class DuckiebotEnv(gym.Env):
         # Draw the image to the rendering window
         width = self.img.shape[1]
         height = self.img.shape[0]
-        imgData = pyglet.image.ImageData(width, height, "RGB", self.img.tobytes(), pitch=width * 3,)
+        imgData = pyglet.image.ImageData(
+            width,
+            height,
+            "RGB",
+            self.img.tobytes(),
+            pitch=width * 3,
+        )
         imgData.blit(0, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
 
         if mode == "human":
